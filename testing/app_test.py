c72c573317e5dc676c973993c0fd7fee550926e2
James <james.kariuki5@student.moringaschool.com>
2025-08-07 08:23:47 +0300

    initial commit

---
+++ testing/app_test.py
@@ -0,0 +1,26 @@
+# testing/app_test.py
+import pytest
+import os
+from os import path
+import subprocess
+
+class TestAppPy:
+    def test_app_py_exists(self):
+        '''
+        exists in lib directory
+        '''
+        assert path.exists("lib/app.py")
+
+    def test_app_py_is_executable(self):
+        '''
+        is executable
+        '''
+        result = subprocess.run(["python3", "lib/app.py"], capture_output=True, text=True)
+        assert result.returncode == 0
+
+    def test_app_py_prints_hello_world(self):
+        '''
+        prints "Hello World! Pass this test, please."
+        '''
+        result = subprocess.run(["python3", "lib/app.py"], capture_output=True, text=True)
+        assert result.stdout.strip() == "Hello World! Pass this test, please."
--- testing/test_app.py
+++
@@ -1,4 +0,0 @@
-from os import path
-
-def test_app_py_exists():
-    assert path.exists("lib/app.py")
